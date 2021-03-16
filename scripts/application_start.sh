16397904952c368770d7650e3f235535f400faa6
stacy1999 <anastasiapolinesi@gmail.com>
2021-03-16 17:21:38 +0000

    Added echo statements

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,18 @@
 cd ~
-echo "installing maven";
+echo 'installing maven'
 sudo yum install -y maven
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-echo "installing node";
+echo 'installing node'
 nvm install node
-echo "installing postgresql";
+echo 'installing postgresql'
 sudo yum install -y postgresql postgresql-server
 sudo service postgresql-setup initdb
-echo "starting postgresql";
+echo 'starting postgresql'
 sudo service postgresql start
-echo "creating user";
+echo 'creating user'
 sudo -u postgres createuser -s ec2-user
-echo "creating database";
+echo 'creating database'
 createdb acebook_springboot_development
-echo "starting springboot";
+echo 'starting springboot'
 mvn spring-boot:run
